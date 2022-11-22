7c39eabdb26c995fa37fdf28996a8b2927cf36bb
Hugo Delbegue <hugodelbegue@gmail.com>
2022-11-22 23:40:04 +0100

    add the cards container

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// Vue.js version 2
 // const app = new Vue({
 //     el: '#app',
 //     data: {
@@ -5,10 +6,71 @@
 //     }
 // });
 
-const app = Vue.createApp({
-    data(){
+// data
+const itemsList = [
+    { id: 1, description: 'Grenouille Pithecopus hypochondrialis amphibiens', points: 4, img: './assets/img/grenouille.jpg' },
+    { id: 2, description: 'Papillon Euploea core', points: 6, img: './assets/img/papillon.jpg' },
+    { id: 3, description: 'Fleur Marguerite Mauve africaine', points: 3, img: './assets/img/marguerite.jpg' },
+    { id: 4, description: 'Grand Coquillage de mer', points: 12, img: './assets/img/coquillage.jpg' },
+    { id: 5, description: 'Champignon de Foret d\'automne', points: 15, img: './assets/img/champignon.jpg' },
+    { id: 6, description: 'Koala d\'Australie', points: 7, img: './assets/img/koala.jpg' },
+    { id: 7, description: 'Fleur Strelitzia', points: 13, img: './assets/img/strelitzia.jpg' },
+    { id: 8, description: 'Oiseau qui vole', points: 5, img: './assets/img/oiseau.jpg' },
+    { id: 9, description: 'Escargot dans sa coquille', points: 16, img: './assets/img/escargot.jpg' },
+    { id: 10, description: 'Goutte d\'eau qui tombe', points: 21, img: './assets/img/goutte.jpg' },
+    { id: 11, description: 'Coccinelles Rouge', points: 18, img: './assets/img/coccinelles.jpg' },
+    { id: 12, description: 'Herisson des Forets', points: 11, img: './assets/img/herisson.jpg' },
+    { id: 13, description: 'Tortue de Mer', points: 9, img: './assets/img/tortue.jpg' }
+]
+
+const Home = {
+    template: '#home',
+    name: 'Home',
+    data: () => {
         return {
-            message: 'version 3'
+            itemsList,
         }
     }
-}).mount('#app');
+}
+
+const Main = {
+    template: '<h1>main</h1>',
+    name: 'Main',
+    data: () => {
+        return {
+            itemsList,
+        }
+    }
+}
+
+const Likes = {
+    template: '<h1>likes</h1>',
+    name: 'Likes'
+}
+
+const Basket = {
+    template: '<h1>basket</h1>',
+    name: 'Basket'
+}
+
+// Routeur
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes: [
+        { path: '/', component: Home, name: 'Home' },
+        { path: '/main', component: Main, name: 'Main' },
+        { path: '/likes', component: Likes, name: 'Likes' },
+        { path: '/basket', component: Basket, name: 'Basket' },
+    ]
+});
+
+// Vue.js version 3
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Instance vue.js version 3'
+        }
+    }
+});
+app.use(router);
+app.mount('#app');
---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// data
+const data = [
+    { id: 1, description: 'Grenouille Pithecopus hypochondrialis amphibiens', point: 4, img: './assets/img/grenouille.jpg' },
+    { id: 2, description: 'Papillon Euploea core', point: 6, img: './assets/img/papillon.jpg' },
+    { id: 3, description: 'Fleur Marguerite Mauve africaine', point: 3, img: './assets/img/marguerite.jpg' },
+    { id: 4, description: 'Grand Coquillage de mer', point: 12, img: './assets/img/coquillage.jpg' },
+    { id: 5, description: 'Champignon de Foret d\'automne', point: 15, img: './assets/img/champignon.jpg' },
+    { id: 6, description: 'Koala d\'Australie', point: 7, img: './assets/img/koala.jpg' },
+    { id: 7, description: 'Fleur Strelitzia', point: 13, img: './assets/img/strelitzia.jpg' },
+    { id: 8, description: 'Oiseau qui vole', point: 5, img: './assets/img/oiseau.jpg' },
+    { id: 9, description: 'Escargot dans sa coquille', point: 16, img: './assets/img/escargot.jpg' },
+    { id: 10, description: 'Goutte d\'eau qui tombe', point: 21, img: './assets/img/goutte.jpg' },
+    { id: 11, description: 'Coccinelles Rouge', point: 18, img: './assets/img/coccinelles.jpg' },
+    { id: 12, description: 'Herisson des Forets', point: 11, img: './assets/img/herisson.jpg' },
+    { id: 13, description: 'Tortue de Mer', point: 9, img: './assets/img/tortue.jpg' },
+]
