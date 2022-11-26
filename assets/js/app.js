a61dc17ffa0cd19eb51cfc0c901ab05882d9fde7
Hugo Delbegue <hugodelbegue@gmail.com>
2022-11-26 23:15:28 +0100

    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,91 +1,18 @@
-// Vue.js version 2
-// const app = new Vue({
-//     el: '#app',
-//     data: {
-//         message: 'hello world mother fucker!'
-//     }
-// });
-
 // data
 const itemsList = [
-  {
-    id: 1,
-    description: "Grenouille Pithecopus hypochondrialis amphibiens",
-    points: 4,
-    img: "./assets/img/grenouille.jpg",
-  },
-  {
-    id: 2,
-    description: "Papillon Euploea core",
-    points: 6,
-    img: "./assets/img/papillon.jpg",
-  },
-  {
-    id: 3,
-    description: "Fleur Marguerite Mauve africaine",
-    points: 3,
-    img: "./assets/img/marguerite.jpg",
-  },
-  {
-    id: 4,
-    description: "Grand Coquillage de mer",
-    points: 12,
-    img: "./assets/img/coquillage.jpg",
-  },
-  {
-    id: 5,
-    description: "Champignon de Foret d'automne",
-    points: 15,
-    img: "./assets/img/champignon.jpg",
-  },
-  {
-    id: 6,
-    description: "Koala d'Australie",
-    points: 7,
-    img: "./assets/img/koala.jpg",
-  },
-  {
-    id: 7,
-    description: "Fleur Strelitzia",
-    points: 13,
-    img: "./assets/img/strelitzia.jpg",
-  },
-  {
-    id: 8,
-    description: "Oiseau qui vole",
-    points: 5,
-    img: "./assets/img/oiseau.jpg",
-  },
-  {
-    id: 9,
-    description: "Escargot dans sa coquille",
-    points: 16,
-    img: "./assets/img/escargot.jpg",
-  },
-  {
-    id: 10,
-    description: "Goutte d'eau qui tombe",
-    points: 21,
-    img: "./assets/img/goutte.jpg",
-  },
-  {
-    id: 11,
-    description: "Coccinelles Rouge",
-    points: 18,
-    img: "./assets/img/coccinelles.jpg",
-  },
-  {
-    id: 12,
-    description: "Herisson des Forets",
-    points: 11,
-    img: "./assets/img/herisson.jpg",
-  },
-  {
-    id: 13,
-    description: "Tortue de Mer",
-    points: 9,
-    img: "./assets/img/tortue.jpg",
-  },
+  { id: 1, description: "Grenouille Pithecopus hypochondrialis amphibiens", points: 4, img: "./assets/img/grenouille.jpg" },
+  { id: 2, description: "Papillon Euploea core", points: 6, img: "./assets/img/papillon.jpg" },
+  { id: 3, description: "Fleur Marguerite Mauve africaine", points: 3, img: "./assets/img/marguerite.jpg" },
+  { id: 4, description: "Grand Coquillage de mer", points: 12, img: "./assets/img/coquillage.jpg" },
+  { id: 5, description: "Champignon de Foret d'automne", points: 15, img: "./assets/img/champignon.jpg" },
+  { id: 6, description: "Koala d'Australie", points: 7, img: "./assets/img/koala.jpg" },
+  { id: 7, description: "Fleur Strelitzia", points: 13, img: "./assets/img/strelitzia.jpg" },
+  { id: 8, description: "Oiseau qui vole", points: 5, img: "./assets/img/oiseau.jpg" },
+  { id: 9, description: "Escargot dans sa coquille", points: 16, img: "./assets/img/escargot.jpg" },
+  { id: 10, description: "Goutte d'eau qui tombe", points: 21, img: "./assets/img/goutte.jpg" },
+  { id: 11, description: "Coccinelles Rouge", points: 18, img: "./assets/img/coccinelles.jpg" },
+  { id: 12, description: "Herisson des Forets", points: 11, img: "./assets/img/herisson.jpg" },
+  { id: 13, description: "Tortue de Mer", points: 9, img: "./assets/img/tortue.jpg" }
 ];
 
 const Home = {
@@ -95,35 +22,54 @@ const Home = {
     return {
       itemsList,
       searchKey: "",
-      liked: []
+      liked: [],
+      card: [],
+      total: []
     };
   },
   computed: {
     filteredList() {
       return this.itemsList.filter((items) => {
-        console.log(this.searchKey);
         return items.description
           .toLowerCase()
           .includes(this.searchKey.toLowerCase());
       });
     },
     getLikeCookie() {
-        let cookiesValue = JSON.parse($cookies.get('like'));
-        cookiesValue == null ? this.liked = [] : this.liked = cookiesValue;
-        // if (cookiesValue == null){
-        //     this.liked = [];
-        // } else {
-        //     this.likes = cookiesValue;
-        // }
+      let cookiesValue = JSON.parse($cookies.get('like'));
+      cookiesValue == null ? this.liked = [] : this.liked = cookiesValue;
+      // if (cookiesValue == null){
+      //     this.liked = [];
+      // } else {
+      //     this.likes = cookiesValue;
+      // }
+    },
+    getTotalPoints() {
+      return console.log('hello');
     }
   },
   methods: {
     setLikeCookie() {
-        document.addEventListener('input', () => {
-            setTimeout(() => {
-                $cookies.set('like', JSON.stringify(this.liked))
-            }, 300);
-        });
+      document.addEventListener('input', () => {
+        setTimeout(() => {
+          $cookies.set('like', JSON.stringify(this.liked))
+        }, 300);
+      });
+    },
+    addToCard(items) {
+      for (let i = 0; i < this.card.length; i++){
+        if (this.card[i].id == items.id){
+          return this.card[i].quantity++;
+        }
+      }
+      this.card.push({
+        id: items.id,
+        description: items.description,
+        points: items.points,
+        img: items.img,
+        quantity: 1
+      });
+      console.log('push')
     }
   },
   mounted: () => {
@@ -147,7 +93,7 @@ const Basket = {
   name: "Basket",
 };
 
-// Routeur
+// Routeur vue 3 //
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes: [
@@ -158,7 +104,17 @@ const router = VueRouter.createRouter({
   ],
 });
 
-// Vue.js version 3
+// Router vue 2 //
+// const router = new VueRouter({
+//   routes: [
+//     { path: "/", component: Home, name: "Home" },
+//     { path: "/main", component: Main, name: "Main" },
+//     { path: "/likes", component: Likes, name: "Likes" },
+//     { path: "/basket", component: Basket, name: "Basket" },
+//   ],
+// });
+
+// Vue.js version 3 //
 const app = Vue.createApp({
   data() {
     return {
@@ -168,3 +124,13 @@ const app = Vue.createApp({
 });
 app.use(router);
 app.mount("#app");
+
+// Vue.js version 2 //
+// const app = new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'Instance vue.js version 2',
+//     },
+//     router
+// });
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,69 @@
-// data
-const data = [
-  {
-    id: 1,
-    description: "Grenouille Pithecopus hypochondrialis amphibiens",
-    point: 4,
-    img: "./assets/img/grenouille.jpg",
-  },
-  {
-    id: 2,
-    description: "Papillon Euploea core",
-    point: 6,
-    img: "./assets/img/papillon.jpg",
-  },
-  {
-    id: 3,
-    description: "Fleur Marguerite Mauve africaine",
-    point: 3,
-    img: "./assets/img/marguerite.jpg",
-  },
-  {
-    id: 4,
-    description: "Grand Coquillage de mer",
-    point: 12,
-    img: "./assets/img/coquillage.jpg",
-  },
-  {
-    id: 5,
-    description: "Champignon de Foret d'automne",
-    point: 15,
-    img: "./assets/img/champignon.jpg",
-  },
-  {
-    id: 6,
-    description: "Koala d'Australie",
-    point: 7,
-    img: "./assets/img/koala.jpg",
-  },
-  {
-    id: 7,
-    description: "Fleur Strelitzia",
-    point: 13,
-    img: "./assets/img/strelitzia.jpg",
-  },
-  {
-    id: 8,
-    description: "Oiseau qui vole",
-    point: 5,
-    img: "./assets/img/oiseau.jpg",
-  },
-  {
-    id: 9,
-    description: "Escargot dans sa coquille",
-    point: 16,
-    img: "./assets/img/escargot.jpg",
-  },
-  {
-    id: 10,
-    description: "Goutte d'eau qui tombe",
-    point: 21,
-    img: "./assets/img/goutte.jpg",
-  },
-  {
-    id: 11,
-    description: "Coccinelles Rouge",
-    point: 18,
-    img: "./assets/img/coccinelles.jpg",
-  },
-  {
-    id: 12,
-    description: "Herisson des Forets",
-    point: 11,
-    img: "./assets/img/herisson.jpg",
-  },
-  {
-    id: 13,
-    description: "Tortue de Mer",
-    point: 9,
-    img: "./assets/img/tortue.jpg",
-  },
-];
-
-// animation icons cards
-inputIcon = document.querySelectorAll(".like__container > input");
-inputIcon.forEach((input) => {
-  input.addEventListener("change", (e) => {
-    if (e.currentTarget.checked == true) {
-      console.log("yessssss");
-    } else if (e.currentTarget.checked == false) {
-      console.log("nooooo");
-    }
+// animation of the basket button //
+// get container button
+const card = document.querySelectorAll('.basket__container').forEach((cc) => {
+  // get button
+  cc.querySelectorAll('.basket__button').forEach((bc) => {
+    bc.addEventListener('click', () => {
+      // set the properties on click
+      // button on
+      if (bc.value === 'on') {
+        bc.value = 'off';
+        bc.style.transition = "all .25s";
+        bc.style.transform = "scale(1)"
+        bc.querySelectorAll('.bi-cart-fill').forEach((bcf) => {
+          bcf.style.color = "initial"
+        })
+        // button off
+      } else {
+        bc.value = 'on';
+        bc.style.transition = "all .25s";
+        bc.style.transform = "scale(1.2)"
+        bc.querySelectorAll('.bi-cart-fill').forEach((bcf) => {
+          bcf.style.color = "#019875"
+        })
+      }
+    })
   });
 });
+
+// animation of the likes notif //
+
+let input = document.querySelectorAll(".checkbox__state")
+let notif = document.getElementById("likes__notif");
+
+const checkboxcheck = () => {
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const setNotif = () => {
+  if (checkboxcheck()) {
+    notif.style.visibility = "visible";
+    notif.style.opacity = "1";
+  } else {
+    notif.style.visibility = "hidden";
+    notif.style.opacity = "0";
+  }
+}
+
+setNotif();
+document.addEventListener('click', setNotif)
+
+    // .forEach((cs) => {
+    //   cs.addEventListener('input', (e) => {
+    //     if (e.currentTarget.checked) {
+    //       console.log(e.currentTarget.checked = true)
+    //       notif.style.visibility = "visible";
+    //       notif.style.opacity = "1";
+    //       console.log('oui')
+    //     } else {
+    //       notif.style.visibility = "hidden";
+    //       notif.style.opacity = "0";
+    //       console.log('non')
+    //     }
+    //   })
+    // })
